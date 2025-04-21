05f13431085c3eedbd14a0c12ee5d49315be8680
Jecky <jecky.tiongco@gmail.com>
2025-04-21 11:23:39 +0800

    Adding the tailwindcss repository

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('@tailwindcss/vite').Config} */
+
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        animation: {
+          'fade-in': 'fadeIn 0.10s ease-in-out',
+        },
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: '0' },
+            '100%': { opacity: '1' },
+          },
+        },
+        fontFamily: {
+          sans: ['Jost', 'sans-serif'],
+        },
+
+      },
+    },
+  }
